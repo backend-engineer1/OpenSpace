backend-engineer1/OpenSpace

commit f48400523c7e8c7ed9b5f24c073ce6a5f2805119

A Chunk not visible to the camera wants to get merged. Cleaned up ChunkRender render signature

modules/globebrowsing/globes/chunk.cpp
@@ -90,8 +90,6 @@ namespace openspace {
         const Ellipsoid& ellipsoid = _owner->ellipsoid();
 
         
-
-        
         
         const int maxHeight = 8700; // should be read from gdal dataset or mod file
 
@@ -100,10 +98,11 @@ namespace openspace {
             _isVisible &= FrustumCuller::isVisible(myRenderData, _surfacePatch, ellipsoid, maxHeight);
         }
 
-
         if (_owner->doHorizonCulling) {
             _isVisible &= HorizonCuller::isVisible(myRenderData, _surfacePatch, ellipsoid, maxHeight);
         }
+
+        if (!_isVisible) return WANT_MERGE;
 
 
         Vec3 cameraPosition = myRenderData.camera.position().dvec3();

modules/globebrowsing/globes/chunknode.cpp
@@ -96,10 +96,11 @@ bool ChunkNode::internalUpdateChunkTree(const RenderData& data) {
         }
 
         // check if all children requested merge
-        if (requestedMergeMask == 0xf) {
+        if (requestedMergeMask == 0xf && _chunk.update(data)) {
             merge();
 
-            // re-run this method on this, now that this is a leaf node
+            // re-run internalUpdateChunkTree on this, now that this is a leaf node
+            // OBS, this may currently cause a split() again ...
             return internalUpdateChunkTree(data);
         }
         return false;
@@ -111,8 +112,7 @@ void ChunkNode::internalRender(const RenderData& data) {
     if (isLeaf()) {
         if (_chunk.isVisible()) {
             ChunkRenderer& patchRenderer = _chunk.owner()->getPatchRenderer();
-            patchRenderer.renderChunk(_chunk, _chunk.owner()->ellipsoid(), data);
-            //patchRenderer.renderPatch(_chunk.surfacePatch, data, _chunk.owner->ellipsoid(), _chunk.index);
+            patchRenderer.renderChunk(_chunk, data);
             ChunkNode::renderedPatches++;
         }
     }
@@ -126,7 +126,6 @@ void ChunkNode::internalRender(const RenderData& data) {
 void ChunkNode::split(int depth) {
     if (depth > 0 && isLeaf()) {
         for (size_t i = 0; i < 4; i++) {
-            
             Chunk chunk(_chunk.owner(), _chunk.index().child((Quad)i), _chunk.owner()->initChunkVisible);
             _children[i] = std::unique_ptr<ChunkNode>(new ChunkNode(chunk, this));
         }
@@ -146,6 +145,7 @@ void ChunkNode::merge() {
         }
         _children[i] = nullptr;
     }
+    
     ghoul_assert(isLeaf(), "ChunkNode must be leaf after merge");
 }
 

modules/globebrowsing/rendering/patchrenderer.cpp
@@ -24,6 +24,7 @@
 
 
 #include <modules/globebrowsing/rendering/patchrenderer.h>
+#include <modules/globebrowsing/globes/chunkedlodglobe.h>
 #include <modules/globebrowsing/meshes/clipmapgrid.h>
 
 // open space includes
@@ -35,6 +36,7 @@
 #include <ghoul/misc/assert.h>
 #include <ghoul/opengl/texture.h>
 #include <ghoul/opengl/textureunit.h>
+
 
 
 #define _USE_MATH_DEFINES
@@ -108,24 +110,16 @@ namespace openspace {
         _programObjectLocalRendering->setIgnoreSubroutineUniformLocationError(IgnoreError::Yes);
     }
 
-    void ChunkRenderer::renderChunk(
-        const Chunk& chunk,
-        const Ellipsoid& ellipsoid, 
-        const RenderData& data) 
-    {
+    void ChunkRenderer::renderChunk(const Chunk& chunk, const RenderData& data) {
         if (chunk.index().level < 9) {
-            renderChunkGlobally(chunk, ellipsoid, data);
+            renderChunkGlobally(chunk, data);
         }
         else {
-            renderChunkLocally(chunk, ellipsoid, data);
+            renderChunkLocally(chunk, data);
         }
     }
 
-    void ChunkRenderer::renderChunkGlobally(
-        const Chunk& chunk,
-        const Ellipsoid& ellipsoid,
-        const RenderData& data)
-    {
+    void ChunkRenderer::renderChunkGlobally(const Chunk& chunk, const RenderData& data){
         using namespace glm;
 
         // TODO : Model transform should be fetched as a matrix directly.
@@ -176,6 +170,7 @@ namespace openspace {
 
         Geodetic2 swCorner = chunk.surfacePatch().southWestCorner();
         auto patchSize = chunk.surfacePatch().size();
+        const Ellipsoid& ellipsoid = chunk.owner()->ellipsoid();
         _programObjectGlobalRendering->setUniform("modelViewProjectionTransform", modelViewProjectionTransform);
         _programObjectGlobalRendering->setUniform("minLatLon", vec2(swCorner.toLonLatVec2()));
         _programObjectGlobalRendering->setUniform("lonLatScalingFactor", vec2(patchSize.toLonLatVec2()));
@@ -193,11 +188,7 @@ namespace openspace {
     }
 
 
-    void ChunkRenderer::renderChunkLocally(
-        const Chunk& chunk,
-        const Ellipsoid& ellipsoid,
-        const RenderData& data)
-    {
+    void ChunkRenderer::renderChunkLocally(const Chunk& chunk, const RenderData& data){
         using namespace glm;
 
         // TODO : Model transform should be fetched as a matrix directly.
@@ -247,6 +238,8 @@ namespace openspace {
         Geodetic2 se = chunk.surfacePatch().southEastCorner();
         Geodetic2 nw = chunk.surfacePatch().northWestCorner();
         Geodetic2 ne = chunk.surfacePatch().northEastCorner();
+
+        const Ellipsoid& ellipsoid = chunk.owner()->ellipsoid();
 
         // Get model space positions of the four control points
         Vec3 patchSwModelSpace = ellipsoid.cartesianSurfacePosition(sw);

modules/globebrowsing/rendering/patchrenderer.h
@@ -83,19 +83,13 @@ namespace openspace {
         ChunkRenderer(shared_ptr<Grid> grid, 
                       shared_ptr<TileProviderManager> tileProviderManager);
 
-        void ChunkRenderer::renderChunk(
-            const Chunk& chunk,
-            const Ellipsoid& ellipsoid,
-            const RenderData& data);
+        void ChunkRenderer::renderChunk(const Chunk& chunk, const RenderData& data);
+
     private:
         void ChunkRenderer::renderChunkGlobally(
-            const Chunk& chunk,
-            const Ellipsoid& ellipsoid,
-            const RenderData& data);
+            const Chunk& chunk, const RenderData& data);
         void ChunkRenderer::renderChunkLocally(
-            const Chunk& chunk,
-            const Ellipsoid& ellipsoid,
-            const RenderData& data);
+            const Chunk& chunk, const RenderData& data);
         shared_ptr<Grid> _grid;
     };
 

commit 2d82911eed3c8989a9c142d2a3498a701d78cfc0

Fixed bug introduced in commit f484005... due to enum Chunk::Status was implicitely casted to bool. Changed enum to class

modules/globebrowsing/globes/chunk.cpp
@@ -102,8 +102,9 @@ namespace openspace {
             _isVisible &= HorizonCuller::isVisible(myRenderData, _surfacePatch, ellipsoid, maxHeight);
         }
 
-        if(!_isVisible && owner()->mergeInvisible)
-            return WANT_MERGE;
+        if (!_isVisible && owner()->mergeInvisible) {
+            return Status::WANT_MERGE;
+        }
 
 
         Vec3 cameraPosition = myRenderData.camera.position().dvec3();
@@ -125,9 +126,9 @@ namespace openspace {
         // clamp level
         desiredLevel = glm::clamp(desiredLevel, _owner->minSplitDepth, _owner->maxSplitDepth);
 
-        if (desiredLevel < _index.level) return WANT_MERGE;
-        else if (_index.level < desiredLevel) return WANT_SPLIT;
-        else return DO_NOTHING;
+        if (desiredLevel < _index.level) return Status::WANT_MERGE;
+        else if (_index.level < desiredLevel) return Status::WANT_SPLIT;
+        else return Status::DO_NOTHING;
     }
 
     void Chunk::render(const RenderData& data) const {

modules/globebrowsing/globes/chunk.h
@@ -44,7 +44,7 @@ namespace openspace {
     class Chunk {
     public:
 
-        enum Status{
+        enum class Status{
             DO_NOTHING,
             WANT_MERGE,
             WANT_SPLIT,

modules/globebrowsing/globes/chunknode.cpp
@@ -74,13 +74,12 @@ bool ChunkNode::updateChunkTree(const RenderData& data) {
 
     if (isLeaf()) {
         Chunk::Status status = _chunk.update(data);
-        if (status == Chunk::WANT_SPLIT) {
+        if (status == Chunk::Status::WANT_SPLIT) {
             split();
         }
-        return status == Chunk::WANT_MERGE;
+        return status == Chunk::Status::WANT_MERGE;
     }
     else {
-        
         char requestedMergeMask = 0;
         for (int i = 0; i < 4; ++i) {
             if (_children[i]->updateChunkTree(data)) {
@@ -88,14 +87,13 @@ bool ChunkNode::updateChunkTree(const RenderData& data) {
             }
         }
 
-        // check if all children requested merge
-        if (requestedMergeMask == 0xf && _chunk.update(data)) {
-            merge();
+        bool allChildrenWantsMerge = requestedMergeMask == 0xf;
+        bool thisChunkWantsSplit = _chunk.update(data) == Chunk::Status::WANT_SPLIT;
 
-            // re-run updateChunkTree on this, now that this is a leaf node
-            // OBS, this may currently cause a split() again ...
-            return updateChunkTree(data);
+        if (allChildrenWantsMerge && !thisChunkWantsSplit) {
+            merge();
         }
+
         return false;
     }
 }